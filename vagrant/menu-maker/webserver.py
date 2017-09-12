022136cb4e8e7d25bb657a4cee1e8e9e4d00d56d
Patrick Roche <patrick.l.roche@gmail.com>
2017-09-12 18:23:47 -0400

    Feat create new restaurants

--- vagrant/menu-maker/webserver.py
+++ vagrant/menu-maker/webserver.py
@@ -14,12 +14,27 @@ class webserverHandler(BaseHTTPRequestHandler):
 
     def do_GET(self):
         try:
-            if self.path.endswith("/restaurants"):
-                restaurants = session.query(Restaurant).all()
-                output = ""
+            if self.path.endswith("/restaurants/new"):
                 self.send_response(200)
                 self.send_header("Content-type", "text/html")
                 self.end_headers()
+                output = "<html><body>"
+                output += "<h1>Make a New Restaurant</h1>"
+                output += "<form method='POST' enctype='multipart/form-data' "
+                output += "action='/restaurants/new'>"
+                output += "<input name='newRestaurantName' type='text' "
+                output += "placeholder='New Restaurant Name'>"
+                output += "<input type='submit' value='Create'>"
+                output += "</form></body></html>"
+                self.wfile.write(output)
+                return
+            if self.path.endswith("/restaurants"):
+                self.send_response(200)
+                self.send_header("Content-type", "text/html")
+                self.end_headers()
+                restaurants = session.query(Restaurant).all()
+                output = "<a href='/restaurants/new'> "
+                output += "Make a New Restaurant Here</a><br><br>"
                 output += "<html><body>"
                 for restaurant in restaurants:
                     output += restaurant.name + "<br>"
@@ -28,13 +43,29 @@ class webserverHandler(BaseHTTPRequestHandler):
                 output += "</body></html>"
                 self.wfile.write(output)
                 return
-
         except IOError:
-            self.send_error(404, "File Not Found %s" % self.path)
+            self.send_error(404, "File Not Found: %s" % self.path)
+
+    def do_POST(self):
+        try:
+            if self.path.endswith("/restaurants/new"):
+                ctype, pdict = cgi.parse_header(
+                    self.headers.getheader("content-type"))
+                if ctype == "multipart/form-data":
+                    fields = cgi.parse_multipart(self.rfile, pdict)
+                    messagecontent = fields.get("newRestaurantName")
+                    newRestaurant = Restaurant(name=messagecontent[0])
+                    session.add(newRestaurant)
+                    session.commit()
+                    self.send_response(301)
+                    self.send_header("Content-type", "text/html")
+                    self.send_header("Location", "/restaurants")
+                    self.end_headers()
+        except:
+            pass
 
 
 def main():
-
     try:
         port = 8080
         server = HTTPServer(("", port), webserverHandler)
@@ -42,7 +73,6 @@ def main():
         message += "/restaurants in your browser"
         print message
         server.serve_forever()
-
     except KeyboardInterrupt:
         print "\nStopping web server..."
         server.socket.close()
