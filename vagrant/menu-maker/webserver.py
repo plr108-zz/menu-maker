cf027cb8f598a2739265f3cb8f087c69e134ff1d
Patrick Roche <patrick.l.roche@gmail.com>
2017-09-12 17:12:29 -0400

    Feat list out all restaurants

--- vagrant/menu-maker/webserver.py
+++ vagrant/menu-maker/webserver.py
@@ -1,87 +1,50 @@
 from BaseHTTPServer import BaseHTTPRequestHandler, HTTPServer
 import cgi
+from database_setup import Base, Restaurant, MenuItem
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+engine = create_engine("sqlite:///restaurantmenu.db")
+Base.metadata.bind = engine
+DBSession = sessionmaker(bind=engine)
+session = DBSession()
 
 
 class webserverHandler(BaseHTTPRequestHandler):
+
     def do_GET(self):
         try:
-            if self.path.endswith("/hello"):
+            if self.path.endswith("/restaurants"):
+                restaurants = session.query(Restaurant).all()
+                output = ""
                 self.send_response(200)
-                self.send_header('Content-type', 'text/html')
+                self.send_header("Content-type", "text/html")
                 self.end_headers()
-                output = "<html><body>"
-                output += "<h1>Hello!</h1>"
-                output += "<form method='POST' enctype='multipart/form-data'"
-                output += "action='/hello'>"
-                output += "<h2>What would you like me to say?</h2>"
-                output += "<input name='message' type='text'>"
-                output += "<input type='submit' value='Submit'>"
-                output += "</form>"
+                output += "<html><body>"
+                for restaurant in restaurants:
+                    output += restaurant.name + "<br>"
                 output += "</body></html>"
                 self.wfile.write(output)
-                print output
-                return
-            if self.path.endswith("/hola"):
-                self.send_response(200)
-                self.send_header('Content-type', 'text/html')
-                self.end_headers()
-
-                output = "<html><body>"
-                output += "&#161Hola! <a href = '/hello' >Back to Hello</a>"
-                output += '''<form method='POST' enctype='multipart/form-data'
-                action='/hello'>
-                <h2>What would you like me to say?</h2>
-                <input name='message' type='text'>
-                <input type='submit' value='Submit'>
-                </form>'''
-                output += "</body></html>"
-                self.wfile.write(output)
-                print output
                 return
 
         except IOError:
             self.send_error(404, "File Not Found %s" % self.path)
 
-    def do_POST(self):
-        try:
-            self.send_response(301)
-            self.end_headers()
-
-            ctype, pdict = cgi.parse_header(
-                self.headers.getheader('content-type')
-            )
-            if ctype == 'multipart/form-data':
-                fields = cgi.parse_multipart(self.rfile, pdict)
-                messagecontent = fields.get('message')
-
-                output = "<html><body>"
-                output += "<h2>Okay, how about this:</h2>"
-                output += "<h1>%s<h1>" % messagecontent[0]
-                output += '''<form method='POST' enctype='multipart/form-data'
-                    action='/hello'>
-                    <h2>What would you like me to say?</h2>
-                    <input name='message' type='text'>
-                    <input type='submit' value='Submit'>
-                    </form>'''
-                output += "</body></html>"
-                self.wfile.write(output)
-                print output
-
-        except:
-            pass
-
 
 def main():
+
     try:
         port = 8080
-        server = HTTPServer(('', port), webserverHandler)
-        print "Web server running on port %s" % port
+        server = HTTPServer(("", port), webserverHandler)
+        message = "Web server running...\nOpen localhost:%s" % port
+        message += "/restaurants in your browser"
+        print message
         server.serve_forever()
 
     except KeyboardInterrupt:
-        print "\n^C entered, stopping web server..."
+        print "\nStopping web server..."
         server.socket.close()
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
