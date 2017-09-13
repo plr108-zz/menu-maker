eca6ec15b5c8abeebb1445a12d24ff02609e6bd0
Patrick Roche <patrick.l.roche@gmail.com>
2017-09-13 08:12:57 -0400

    Feat edit restaurants

--- vagrant/menu-maker/webserver.py
+++ vagrant/menu-maker/webserver.py
@@ -28,6 +28,24 @@ class webserverHandler(BaseHTTPRequestHandler):
                 output += "</form></body></html>"
                 self.wfile.write(output)
                 return
+            if self.path.endswith("/edit"):
+                restaurantIDPath = self.path.split("/")[2]
+                myRestaurantQuery = session.query(Restaurant).filter_by(
+                    id=restaurantIDPath).one()
+                if myRestaurantQuery:
+                    self.send_response(200)
+                    self.send_header("Content-type", "text/html")
+                    self.end_headers()
+                    output = "<html><body><h1>%s" % myRestaurantQuery.name
+                    output += "</h1><form method='POST' "
+                    output += "enctype='multipart/form-data' action = "
+                    output += "'/restaurants/%s/edit'>" % restaurantIDPath
+                    output += "<input name='newRestaurantName' type='text' "
+                    output += "placeholder='%s'>" % myRestaurantQuery.name
+                    output += "<input type='submit' value='Rename'>"
+                    output += "</form></body></html>"
+                self.wfile.write(output)
+                return
             if self.path.endswith("/restaurants"):
                 self.send_response(200)
                 self.send_header("Content-type", "text/html")
@@ -37,9 +55,11 @@ class webserverHandler(BaseHTTPRequestHandler):
                 output += "Make a New Restaurant Here</a><br><br>"
                 output += "<html><body>"
                 for restaurant in restaurants:
+                    edit_path = '/restaurants/%s/edit' % restaurant.id
                     output += restaurant.name + "<br>"
-                    output += "<a href='#'>Edit</a><br>"
-                    output += "<a href='#'>Delete</a><br><br><br>"
+                    output += "<a href='%s'>" % edit_path
+                    output += "Edit</a><br><a href='#'>Delete</a>"
+                    output += "<br><br><br>"
                 output += "</body></html>"
                 self.wfile.write(output)
                 return
@@ -48,6 +68,23 @@ class webserverHandler(BaseHTTPRequestHandler):
 
     def do_POST(self):
         try:
+            if self.path.endswith("/edit"):
+                ctype, pdict = cgi.parse_header(
+                    self.headers.getheader("content-type"))
+                if ctype == "multipart/form-data":
+                    fields = cgi.parse_multipart(self.rfile, pdict)
+                    messagecontent = fields.get("newRestaurantName")
+                    restaurantIDPath = self.path.split("/")[2]
+                    myRestaurantQuery = session.query(Restaurant).filter_by(
+                        id=restaurantIDPath).one()
+                    if myRestaurantQuery != []:
+                        myRestaurantQuery.name = messagecontent[0]
+                        session.add(myRestaurantQuery)
+                        session.commit()
+                        self.send_response(301)
+                        self.send_header("Content-type", "text/html")
+                        self.send_header("Location", "/restaurants")
+                        self.end_headers()
             if self.path.endswith("/restaurants/new"):
                 ctype, pdict = cgi.parse_header(
                     self.headers.getheader("content-type"))
