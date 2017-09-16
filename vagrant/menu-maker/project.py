e9eea56fd6239594f7012f3af24790409b163828
Patrick Roche <patrick.l.roche@gmail.com>
2017-09-16 12:42:35 -0400

    feat show Restaurant items via Flask

--- vagrant/menu-maker/project.py
+++ vagrant/menu-maker/project.py
@@ -1,11 +1,27 @@
 from flask import Flask
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from database_setup import Base, Restaurant, MenuItem
+
 app = Flask(__name__)
+engine = create_engine('sqlite:///restaurantmenu.db')
+Base.metadata.bind = engine
+DBSession = sessionmaker(bind=engine)
+session = DBSession()
 
 
 @app.route('/')
 @app.route('/hello')
 def HelloWorld():
-    return 'Hello World'
+    restaurant = session.query(Restaurant).first()
+    items = session.query(MenuItem).filter_by(restaurant_id=restaurant.id)
+    output = ''
+    for item in items:
+        output += item.name + '<br>'
+        output += item.price + '<br>'
+        output += item.description + '<br>'
+        output += '<br>'
+    return output
 
 
 if __name__ == '__main__':
