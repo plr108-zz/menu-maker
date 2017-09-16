c6d53075c882aaa1fd78dce0c0205f2d672d58b0
Patrick Roche <patrick.l.roche@gmail.com>
2017-09-16 13:18:10 -0400

    Add menu item routing

--- vagrant/menu-maker/project.py
+++ vagrant/menu-maker/project.py
@@ -37,6 +37,21 @@ def restaurantMenu(restaurant_id):
     return output
 
 
+@app.route('/restaurants/<int:restaurant_id>/new')
+def newMenuItem(restaurant_id):
+    return "page to create a new menu item. Task 1 complete!"
+
+
+@app.route('/restaurants/<int:restaurant_id>/<int:menu_id>/edit')
+def editMenuItem(restaurant_id, menu_id):
+    return "page to edit a menu item. Task 2 complete!"
+
+
+@app.route('/restaurants/<int:restaurant_id>/<int:menu_id>/delete')
+def deleteMenuItem(restaurant_id, menu_id):
+    return "page to delete a menu item. Task 3 complete!"
+
+
 if __name__ == '__main__':
     app.debug = True
     app.run(host='0.0.0.0', port=5000)
