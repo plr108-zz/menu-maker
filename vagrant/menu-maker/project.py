997c468c21e83d27ccaf5f12c899ec63f7e4a0f7
Patrick Roche <patrick.l.roche@gmail.com>
2017-09-16 13:41:36 -0400

    Show menu using template

--- vagrant/menu-maker/project.py
+++ vagrant/menu-maker/project.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, render_template
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from database_setup import Base, Restaurant, MenuItem
@@ -28,13 +28,7 @@ def HelloWorld():
 def restaurantMenu(restaurant_id):
     restaurant = session.query(Restaurant).filter_by(id=restaurant_id).one
     items = session.query(MenuItem).filter_by(restaurant_id=restaurant_id)
-    output = ''
-    for item in items:
-        output += item.name + '<br>'
-        output += item.price + '<br>'
-        output += item.description + '<br>'
-        output += '<br>'
-    return output
+    return render_template('menu.html', restaurant=restaurant, items=items)
 
 
 @app.route('/restaurants/<int:restaurant_id>/new')
