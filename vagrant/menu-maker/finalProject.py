71213b24d9111c94387bf00669a57e0157ed02de
Patrick Roche <patrick.l.roche@gmail.com>
2017-10-09 20:10:54 -0400

    Add final project with base endpoint

---
+++ vagrant/menu-maker/finalProject.py
@@ -0,0 +1,22 @@
+from flask import Flask
+from flask import render_template, request, redirect, url_for, flash, jsonify
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from database_setup import Base, Restaurant, MenuItem
+
+app = Flask(__name__)
+engine = create_engine('sqlite:///restaurantmenu.db')
+Base.metadata.bind = engine
+DBSession = sessionmaker(bind=engine)
+session = DBSession()
+
+
+@app.route('/')
+def showRestaurants():
+    response = 'This page will show all my restaurants'
+    return response
+
+
+if __name__ == '__main__':
+    app.debug = True
+    app.run(host='0.0.0.0', port=8000)
